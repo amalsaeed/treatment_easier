1674703c850004c8082183b4998a0a2efaf60fbb
t.alshammari <t.alshammari@tamkeentech.sa>
2020-03-14 11:40:20 +0300

    check auth

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,21 +8,23 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index() {
-        $user = User::where('id', auth()->user()->id)->first();
+        if(auth()->check()) {
+            $user = User::where('id', auth()->user()->id)->first();
 
-        if(count($user->profile()->get())) {
-            $user->profile()->UpdateOrCreate(
-                ['user_id' => auth()->user()->id], [
-                    'name' => auth()->user()->name
-                ]
-            );
-        } else {
-            $user->profile()->create([
-                    'name'              => auth()->user()->name,
-                    'mobile'            => '9660000000',
-                    'profile_number'    => rand(pow(10, 4-1), pow(10, 4)-1)
-                ]
-            );
+            if (count($user->profile()->get())) {
+                $user->profile()->UpdateOrCreate(
+                    ['user_id' => auth()->user()->id], [
+                        'name' => auth()->user()->name
+                    ]
+                );
+            } else {
+                $user->profile()->create([
+                        'name' => auth()->user()->name,
+                        'mobile' => '9660000000',
+                        'profile_number' => rand(pow(10, 4 - 1), pow(10, 4) - 1)
+                    ]
+                );
+            }
         }
 
         return view('home');
